2d3db8120c37a2a0a7f4aa5a596f0e75b4c3ebe3
LittleJam <kseniaandrasiuk@gmail.com>
2022-01-14 00:24:19 +0300

    toReadable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,62 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    let word = "";
+    let current = number.toString();
+    const numbers = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const numbersDoz = [
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    if (number == 0) {
+        return numbers[number];
+    }
+
+    if (current.length > 2) {
+        word = `${numbers[current[0]]} hundred `;
+        if (current[1] == "0" && current[2] == "0")
+            return word.substr(0, word.length - 1);
+        current = current.substr(1);
+    }
+    if (current >= 10 && current <= 19) {
+        return word + numbers[current];
+    }
+    if (current.length > 1) {
+        if (current[0] == "0") {
+            return word + numbers[current[1]];
+        }
+        word += `${numbersDoz[current[0] - 2]} `;
+        current = current.substr(1); // 0
+    }
+
+    word = current > 0 ? word + numbers[current] : word;
+
+    return (word =
+        word[word.length - 1] === " " ? word.substr(0, word.length - 1) : word);
+};
